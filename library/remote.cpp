ca167f0ad238a5f8078229223a6804df76cdb0f6
Wouter van Ooijen <wouter.vanooijen@hu.nl>
2017-09-24 12:39:28 +0200

    cleanup

--- library/remote.cpp
+++
@@ -1,8 +0,0 @@
-#include "lemonator_proxy.hpp"
-#include "lemonator_application.hpp"
-
-int main( void ){	
-   auto hw = lemonator_proxy();
-   auto app = lemonator_application( hw );
-   app.run();
-}
