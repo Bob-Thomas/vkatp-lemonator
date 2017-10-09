410026038edd314a7a632b8b5aa3679cb0d61ba3
Bob Thomas <bobthomas295@gmail.com>
2017-10-09 15:33:54 +0200

    extended pybind proxies to cover lemonator_proxy.hpp

--- pc_python/test.py
+++ pc_python/test.py
@@ -3,9 +3,17 @@ import time
 
 print( "Python interface demo running" )
 hw = lemonator.lemonator( 2 )
-led = hw.led_yellow
+
+class led_yellow():
+    def __init__(self, led):
+        self.led = led
+    def set(self, v):
+        self.led.set(v)
+
+h = heater(hw.heater)
+
 while 1:
-   led.set( 1 )
-   time.sleep( 0.5 )
-   led.set( 0 )
-   time.sleep( 0.5 )
+    yellow.set(1)
+    time.sleep(1)
+    yellow.set(0)
+    time.sleep(1)
